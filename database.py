66dbe440009d61f5cb06f4b7846fc549ea3e070b
Molly Clabots <molly.clabots@gmail.com>
2020-02-13 19:22:53 -0600

    Adding an initializing method to the database file

--- database.py
+++ database.py
@@ -1,3 +1,11 @@
 import peewee
 from database_abc import ArtDB
 from model import Art, Artist
+
+class SQLArtDB(ArtDB):
+
+    db = SqliteDatabase('art.db')
+
+    def __init__(self):
+        db.connect()
+        db.create_tables([Art, Artist])
