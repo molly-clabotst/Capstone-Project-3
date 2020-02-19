cee617af0e751d357876ea1c2db6ba7aff7a755a
Molly Clabots <molly.clabots@gmail.com>
2020-02-19 08:23:25 -0600

    Adding new Validation, making display data more atomic, changing value of available to sold or available

--- database.py
+++ database.py
@@ -12,13 +12,28 @@ class SQLArtDB():
         return data
 
     def insert_art(self, name, artist, price, available):
+        if available.lower() == 'yes':
+            available = 'sold'
+        else:
+            available = 'available'
         art = Art(name=name, artist=artist, price=price, available=available)
         art.save()
 
     def search_art_one(self, artist):
+        # ORM
         data = Art.select().where(Art.artist==artist)
         return data
     
     def search_art_all(self):
+        # ORM
         data = Art.select()
+        return data
+
+    def update(self, name):
+        rows_updated = Art.update(available='sold').where(Art.name==name)
+        return rows_updated
+
+    def search_art_name(self, name):
+        # ORM
+        data = Art.select().where(Art.name==name)
         return data
