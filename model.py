4c77bb5ea6fc0e87e094114d7d4a26439e5b0da3
Molly Clabots <molly.clabots@gmail.com>
2020-02-19 09:14:24 -0600

    Changing the model to have a base model to try and fix operational error with name column

--- model.py
+++ model.py
@@ -6,24 +6,22 @@ db = SqliteDatabase('art.sqlite')
 
 db.connect()
 
-class Artist(Model):
-    name = CharField()
-    email = CharField()
-
+class BaseModel(Model):
     class Meta:
         database = db
+
+class Artist(BaseModel):
+    name = CharField()
+    email = CharField()
 
     def __str__(self):
         return f"The artist {self.name} is contactable at {self.email}."
 
-class Art(Model):
+class Art(BaseModel):
     name = CharField()
     artist = ForeignKeyField(model=Artist, field=name)
     price = CharField()
     available = CharField()
-
-    class Meta:
-        database = db
 
     def __str__(self):
         return f"This pieces name is it was created by {self.artist}. The pieces price is {self.price} and it is {self.availabile}."
